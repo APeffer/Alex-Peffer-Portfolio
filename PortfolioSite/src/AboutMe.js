66a31f592676e6b4385f3d59f6a744e6ea1a83a9
AyyPeff <log.alexpeffer@gmail.com>
2023-08-07 22:44:26 -0500

    About ME!
    added 'About Me' Page, updated navbar functionality to be more in Line
    with react documentation of clickHandlers (i think)

--- PortfolioSite/src/AboutMe.js
+++ PortfolioSite/src/AboutMe.js
@@ -1,8 +1,43 @@
-export default function AboutMe() {
+import React from 'react'
 
-    return (
-        <div>
-            <h1>About Me</h1>
+export default function AboutMe() {
+  return (
+    <div style={{height: '1200px', backgroundColor: '#c3c9b9'}}>
+
+
+        <div className="mb-5 text-center user-select-none" style={{backgroundColor: '#c3c9b9'}}>
+            <div className="p-2 text-center">
+                <h1>About Me</h1>
+                <h5 className=''>My name is Alex, but many people call me Peff.</h5>
+            </div>
+            <div className="p-5 text-center bg-body-tertiary">
+                <div className='container py-4'>
+                    <h4>How'd you get started?</h4>
+                    <p>
+                        I started my software development journey by being interested in programming and having 
+                        a desire to make video games. As of now I have 1 completed published game through 
+                        Unity Game Engine. 
+                    </p>
+                    <p>
+                        To play <strong>Grapple</strong> in your web browser through <i>itch.io </i>
+                        <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="https://ayypeff.itch.io/" target="_blank" rel='noreferrer'>click here</a> (best on mouse and keyboard)
+                    </p>
+                    
+                </div>
+                <div className='container py-4'>
+                    <h4>Why Web?</h4>
+                    <p>I've always loved programming and web was the quickest way that made sense for me to start 
+                        being able to see the results of my code. After realizing that I don't think I'd be interested 
+                        in making someone else's game ideas for them in an office, I decided web was a way that I could 
+                        potentially still work in the video game industry around a familiar environment. Additionally, 
+                        I thought that I might be able to make some fun webapps using APIs from games I enjoy to make 
+                        cool stuff that my friends might find useful.
+                    </p>
+                </div>
+            </div>    
         </div>
-    )
+
+
+    </div>
+  )
 }
--- PortfolioSite/src/navbar.js
+++ PortfolioSite/src/navbar.js
@@ -2,11 +2,36 @@ import React from "react"
 
 function Navbar({onHome, onAboutMe, onResume, onProjects, onPokemonApp}) {
   
+  function clickHandler(e){
+    switch (e.target.value){
+      case 'home':
+        console.log("home")
+        onHome();
+        break;
+      case 'aboutme':
+        console.log("aboutme")
+        onAboutMe();
+        break;
+      case 'resume':
+        console.log("resume")
+        onResume();
+        break;
+      case 'projects':
+        console.log("projects")
+        onProjects();
+        break;
+      case 'pokemonapp':
+        console.log("pokemonapp")
+        onPokemonApp();
+        break;
+    }
+  }
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/#" onClick={onHome}>Alex Peffer</a>
+              <button style={{backgroundColor: 'transparent', border: 'none', padding: '0px', paddingTop: '0px', paddingBottom: '5px'}} className="navbar-brand" onClick={clickHandler} value={'home'}>Alex Peffer</button>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
@@ -14,16 +39,19 @@ function Navbar({onHome, onAboutMe, onResume, onProjects, onPokemonApp}) {
                 
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <button className="nav-link" onClick={onAboutMe}>About Me</button>
+                    <button className="nav-link" onClick={clickHandler} value={'aboutme'}>About Me</button>
                   </li>
                   <li className="nav-item">
-                    <button className="nav-link"  onClick={onResume}>Resume</button>
+                    <button className="nav-link" onClick={clickHandler} value={'resume'}>Resume</button>
                   </li>
                   <li className="nav-item">
-                    <button className="nav-link"  onClick={onProjects}>Projects</button>
+                    <button className="nav-link" onClick={clickHandler} value={'projects'}>Projects</button>
                   </li>
                   <li className="nav-item">
-                    <button className="nav-link"  onClick={onPokemonApp}>Pokemon App</button>
+                    <button className="nav-link" onClick={clickHandler} value={'pokemonapp'}>Pokemon App</button>
+                  </li>
+                  <li className="nav-item">
+                    <button className="nav-link" onClick={clickHandler} value={'test'}>Test</button>
                   </li>
                 </ul>
               </div>
