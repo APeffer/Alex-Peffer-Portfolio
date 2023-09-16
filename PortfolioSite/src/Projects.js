0431ec02b004e8166d39e0d8609f7fb8417602bc
AyyPeff <log.alexpeffer@gmail.com>
2023-09-16 01:41:27 -0500

    tiny bugs

--- PortfolioSite/src/Projects.js
+++ PortfolioSite/src/Projects.js
@@ -1,14 +1,14 @@
-import { useState, Children} from "react"
-import PokemonApp from "./Projects/pokemonApp"
+//import { useState} from "react"
+//import PokemonApp from "./Projects/pokemonApp"
 export default function MyGames() {
-    const [projectFrame, setProjectFrame] = useState(<PokemonApp />)
+    //const [projectFrame, setProjectFrame] = useState(<PokemonApp />)
 
     return (
         <div id="project-container text-center">
             
 
             <div className="text-center" id="projects-buttons">
-            <button><img src="./images/projectthumbnails/pokemonthumbnail.png"/></button>
+            <button><img src="./public/images/projectthumbnails/pokemonthumbnail.png" alt="Pokemon App Icon"/></button>
             </div>
             
 
--- PortfolioSite/src/body.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Body({title, children, view}) {
-  return (
-    <div>
-        {children}
-    </div>
-  )
-}
