137d5e97dac9edea4cff65271bbdac2bbae5f836
AyyPeff <log.alexpeffer@gmail.com>
2023-09-30 18:27:29 -0500

    Fix routing bug

--- PortfolioSite/src/Projects.js
+++ PortfolioSite/src/Projects.js
@@ -17,7 +17,7 @@ export default function MyGames() {
                             <h2 className="project-title">This Site!</h2>
                         </div>
                     </div>
-                    <button className= "project-button-try"><Link className="nav-link" to="">Try</Link></button>
+                    <button className= "project-button-try"><Link className="nav-link" to="..">Try</Link></button>
                     <a href="https://github.com/apeffer" target="_blank" rel="noopener noreferrer">
                         <button className= "project-button-source">
                             <img src="/images/icons/github-mark-white.png" alt="github icon" />
@@ -34,7 +34,7 @@ export default function MyGames() {
                             <h2 className="project-title">Pokemon App</h2>
                         </div>
                     </div>
-                    <button className= "project-button-try"><Link className="nav-link" to="pokemonapp">Try</Link></button>
+                    <button className= "project-button-try"><Link className="nav-link"  to="../pokemonapp">Try</Link></button>
                     <a href="https://github.com/APeffer/Pokemon-App" target="_blank" rel="noopener noreferrer">
                         <button className= "project-button-source">
                             <img src="/images/icons/github-mark-white.png" alt="github icon" />
