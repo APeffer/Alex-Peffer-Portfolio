836a72d8b52f64c33d9b33044f26164656294d14
AyyPeff <log.alexpeffer@gmail.com>
2024-02-09 18:11:59 -0600

    adding github and linkedin links

--- PortfolioSite/src/components/Navbar.js
+++ PortfolioSite/src/components/Navbar.js
@@ -11,15 +11,18 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerMenu">
-            <div className="navbar-nav">
+            <div className="navbar-nav me-auto">
               <Link className="nav-link" to="aboutme">About Me</Link>
               <Link className="nav-link" to="projects">Projects</Link>
               <Link className="nav-link" to="games">Games</Link>
-              <Link className="nav-link" to="contact">Contact</Link>
-
-
+              <a href="https://github.com/APeffer" target="_blank" className="nav-link">Github</a>
+              <a href="https://www.linkedin.com/in/apeffer/" target="_blank" className="nav-link">LinkedIn</a>
+            </div>
+            <div className="navbar-nav ms-auto">
+              <Link className="nav-link" to="contact">Contact</Link> 
             </div>
           </div>
+          
           
         </div>
             
